b60b51bfcdffbe3c88c7f44a2ca12f6b33b105be
leehir <1097304039@qq.com>
2024-10-15 20:24:16 +0800

    add train

--- langchain/stream/data_analysis.py
+++ langchain/stream/data_analysis.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import os
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+# Make folder picker dialog appear on top of other windows
+root.wm_attributes('-topmost', 1)
 
 def get_file_list(suffix, path):
     """
@@ -9,15 +17,24 @@ def get_file_list(suffix, path):
     input_template_all =[]
     input_template_all_path =[]
     for root, dirs, files in os.walk(path, topdown=False):
-        for name in files:tsu
+        for name in files:
             if os.path.splitext(name)[1] == suffix:
                 input_template_all.append(name)
                 input_template_all_path.append(os.path.join(root, name))
     return input_template_all, input_template_all_path
 
-input_folder = st.file_uploader("Choose a folder of files", type=["csv", "png", "jpg"], multiple=True)
-path = input_folder
-_, file_list_xlsx = get_file_list(".xlsx", path)
+# input_folder = st.file_uploader("Choose a folder of files", type=["csv", "png", "jpg"], multiple=True)
+# path = input_folder
+
+
+clicked = st.sidebar.button('Folder Picker')
+
+input_folder = os.getcwd()
+if clicked:
+    input_folder = st.sidebar.text_input('Selected folder:', filedialog.askdirectory(master=root))
+
+_, file_list_xlsx = get_file_list(".xlsx", input_folder)
+if file_list_xlsx:
 
 st.title("数据分析")
 st.sidebar.selectbox("选择当前目录加载的文件", file_list_xlsx)
