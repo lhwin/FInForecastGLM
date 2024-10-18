3448ef3ae6312aab0f2e847b1f500e8b0d4fad6a
leehir <1097304039@qq.com>
2024-10-18 10:38:07 +0800

    chatdoc v1

--- langchain/chatDoc.py
+++ langchain/chatDoc.py
@@ -1,15 +1,48 @@
 from langchain.document_loaders import TextLoader, PyMuPDFLoader
 from langchain.storage import LocalFileStore
+from langchain.chains import StuffDocumentsChain, LLMChain
 # from langchain.document_loaders import GithubFileLoader
 from langchain.text_splitter import TextSplitter, CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+import os
+
 api_key = "sk-uLPxcqRQ4r9eFvDD7225F90eEa0f479dB2D918E6AbBfF934"
 api_base = "https://api.openai99.top/v1"
 
-e_model = OpenAIEmbeddings(api_key=api_key, base_url=api_base)
+os.environ["OPENAI_API_BASE"] = api_base
+os.environ["OPENAI_API_KEY"] = api_key
+
+e_model = OpenAIEmbeddings()
 fs = LocalFileStore("./cache/")
 local_cache = CacheBackedEmbeddings.from_bytes_store(e_model, fs, namespace=e_model.model)
+
+llm = OpenAI(model="gpt-3.5-turbo-16k",
+            temperature=0)
+
+document_prompt = PromptTemplate(input_variable=["page_content"], template="{page_content}")
+
+stuff_prompt_override = """Given this text extracts:
+    __________________________________________
+
+    {context}
+
+    ------------------------------------------
+
+    Please answer the following questions:
+    {query}
+
+    """
+prompt = PromptTemplate(template=stuff_prompt_override,
+                            input_variable=["context", "query"])
+
+llmchain = LLMChain(llm=llm, prompt=prompt)
+
+workchain = StuffDocumentsChain(llm_chain=llmchain, document_prompt=document_prompt,
+                                    document_variable_name="context")
+
 
 def read_and_split(file_name):
 
@@ -24,13 +57,29 @@ def read_and_split(file_name):
     text = text_spliter.split_documents(raw_documents)
     db = FAISS.from_documents(text, local_cache)
 
-    return text
+    db.save_local("./cache/faiss_index")
 
+    return db
+
+def query2gpt(query, db_path="./cache/faiss_index"):
+    embeddings = OpenAIEmbeddings()
+    db = FAISS.load_local(db_path, embeddings, allow_dangerous_deserialization=True)
+
+    doc = db.similarity_search_with_score(query)
+
+    document = [d[0] for d in doc]
+    res = workchain.run({'input_documents':document, "query": query})
+
+    print(res)
+    return res
 def main():
     file_path = "./files/test.txt"
 
-    docs = read_and_split(file_path)
-    print(docs)
+    # docs = read_and_split(file_path)
+
+    query2gpt("依萍怎么了?")
+
+    # print(docs)
 
 
 if __name__ == "__main__":
